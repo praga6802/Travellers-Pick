a3ea3c3658acb8a27e7a3365763164ce2c259ec4
praga6802 <praga.06.08@gmail.com>
2026-09-21 22:19:20 +0530

    update login.js

--- travellers_pick-frontend/js/userlogin.js
+++ travellers_pick-frontend/js/userlogin.js
@@ -16,18 +16,18 @@ async function handleLogin(event) {
             body: JSON.stringify(data),
             credentials: "include",
         });
-        const data = await response.json();
-        console.log(data);
+        const responseData = await response.json();
+        console.log(responseData);
         if (response.ok) {
-            error.textContent = data.message;
+            error.textContent = responseData.message;
             error.classList.remove("failure");
             error.classList.add("success");
             setTimeout(() => (window.location.href = `../index.html`), 2000);
         } else {
-            error.textContent = data.message;
+            error.textContent = responseData.message;
             error.classList.remove("success");
             error.classList.add("failure");
-            console.log("Back end error:", text);
+            console.log("Back end error:", responseData);
         }
     } catch (err) {
         error.textContent = "Network error..Please try again..";
